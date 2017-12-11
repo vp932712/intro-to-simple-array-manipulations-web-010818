8a261bb38a61d759df710f31b2541ba10ed295e5
Vaibhav Patel <vp932712@gmail.com>
2017-12-11 23:07:40 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, value)
+  array.push(value)
+end
+
+def using_unshift(array, value)
+   array.unshift(value)
+ end
+
+ def using_pop(array)
+   array.pop
+ end
+
+ def pop_with_args(array, num)
+   array.pop(num)
+ end
+
+ def using_shift(array)
+   array.shift
+ end
+
+ def shift_with_args(array, num)
+   array.shift(num)
+ end
+
+ def using_concat(array1, array2)
+   array1.concat(array2)
+ end
+ 
