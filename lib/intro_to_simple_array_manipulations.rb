7191a344294a269c5076c57543b5cb374bf8a617
Vaibhav Patel <vp932712@gmail.com>
2017-12-11 23:14:34 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,4 +25,19 @@ def using_unshift(array, value)
  def using_concat(array1, array2)
    array1.concat(array2)
  end
- 
+
+ def using_insert(array, element)
+   array.insert(3, element)
+ end
+ def using_uniq(array)
+   array.uniq
+ end
+ def using_flatten(arrar)
+   arrar.flatten
+ end
+ def using_delete(array, element)
+   array.delete(element)
+ end
+ def using_delete_at(array, num)
+   array.delete_at(num)
+ end
